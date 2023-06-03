c198d83f9c7e4bb50ce08bd69445803c3ab6f003
FikriZaid <88956418+FikriZaid@users.noreply.github.com>
2023-06-03 13:12:56 +0800

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:local_auth/local_auth.dart';
-import 'package:photo_album/screens/dashboard.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:photo_album/screens/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,71 +9,21 @@ void main() async {
   await Firebase.initializeApp();
   await FirebaseAuth.instance.signInAnonymously();  // Sign in anonymously
   
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: MyApp(),
     debugShowCheckedModeBanner: false,
   ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+    
   @override
   Widget build(BuildContext context) {
     return Home();
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final LocalAuthentication localAuth = LocalAuthentication();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        onTap: () async {
-          bool weCanCheckBiometrics = await localAuth.canCheckBiometrics;
-
-          if (weCanCheckBiometrics) {
-            bool authenticated = await localAuth.authenticate(
-              localizedReason: "Authenticate to see your Photo Album.",
-            );
-
-            if (authenticated) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const Dashboard(),
-                ),
-              );
-            }
-          }
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const Icon(
-              Icons.fingerprint,
-              size: 124.0,
-            ),
-            Text(
-              "Touch to Unlock",
-              style: GoogleFonts.passionOne(
-                fontSize: 64.0,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
